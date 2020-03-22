8bbc46a20270c7ea137e2ce6b24aa24492c1dd44
Denis <denis-cher1998@yandex.ru>
2020-03-22 15:56:39 +0300

    complete integrate tests

--- Tests/IntegrateTestsForKurs.py
+++ Tests/IntegrateTestsForKurs.py
@@ -8,7 +8,20 @@ conn = sqlite3.connect("weather_data.db")
 cursor = conn.cursor()
 sense = SenseHat()
 
+def compareDataInFiles(filename, data):
+    file = open(filename,'r')
+    line = file.read()
+    file.close()
+    if line == data:
+        return True
+    else:
+        return False
+
 for i in range(0,10):
+    if i == 5 :
+        os.rename("l1.txt","1.txt")
+        kurs.write_to_lcd()
+        os.rename("1.txt","l1.txt")
     kurs.read_data()
     kurs.write_to_lcd()
     kurs.write_to_db()
@@ -28,4 +41,12 @@ def test_kurs1():
         assert False
 
 def test_kurs2():
-    assert True        
+    temp = sense.temp
+    press = sense.pressure
+    humi = sense.humidity
+    assert compareDataInFiles('l1.txt',"pressure:" + str("%.2f"%round(press/1.33322,2)))
+    assert compareDataInFiles('l2.txt',"temp" + str("%.2f"%round(temp,2)) +
+     "Hum" + str("%.2f"%round(humi,2)))  
+
+def test_kurs3():
+    assert compareDataInFiles('err.log',"ошибка с выводом на экран : error")
