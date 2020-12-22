62ddd4d7af042d25a6b603470547a490f6d6219d
Denis <52039963+deloboiden@users.noreply.github.com>
2020-12-22 12:05:17 +0300

    update project

--- kurs.py
+++ kurs.py
@@ -1,106 +1,105 @@
-#import lcddriver
-import time, datetime
+import lcddriver
+import time
+from datetime import datetime
 import Adafruit_DHT
 import sqlite3
 import schedule
+import sys
 from BMP180 import BMP180
- 
-bmp = BMP180()
-#display = lcddriver.lcd()
-sensor = Adafruit_DHT.DHT22 #sensor is DHT22
-pin = 4                     #connec to to GPIO pin 4
 
-conn = sqlite3.connect("weather_data.db")
-cursor = conn.cursor()
 
-temperature = 0
-pressure = 0
-humidity = 0
-timestamp = 0
+class Sensors():
 
-def sensBMP():
-    pres = 0
-    temp = 0
-    for i in range(0,10):
-        pres += bmp.read_pressure()
-        temp += bmp.read_temperature()
-    pres /= 10
-    temp /= 10
-    return pres,temp
+    def __init__(self, dbPath, dhtPin):
+        self.bmpSensor = BMP180()
+        self.display = None  # lcddriver.lcd()
+        self.dhtSensor = Adafruit_DHT.DHT22
+        self.dhtPin = dhtPin
+        try:
+            self.dbConnection = sqlite3.connect(dbPath)
+        except sqlite3.Error as E:
+            print("failed by {}".format(E))
 
-def sensDHT():
-    humi = 0
-    temp = 0
-    for i in range(0,10):
-         humi1, temp1 = Adafruit_DHT.read_retry(sensor, pin)
-         humi += humi1
-         temp += temp1
-    humi /= 10
-    temp /= 10
-    return humi,temp
+    def sensBMP(self):
+        pres = 0
+        temp = 0
+        for i in range(0, 10):
+            pres += self.bmpSensor.read_pressure()
+            temp += self.bmpSensor.read_temperature()
+        pres /= 10
+        temp /= 10
+        return pres, temp
 
-def create_log(err,code):
-    file = open('err.log','w')
-    if code == 1:
-        file.write("ошибка с чтением с датчиков : ")
-        display.lcd_display_string("err: write data", 1)
-        display.lcd_display_string("from sensors", 2)
-    if code == 2:
-        file.write("ошибка с выводом на экран : ")
-    if code == 3:
-        file.write("ошибка с записью в базу данных : ")
-        display.lcd_display_string("err: write data", 1)
-        display.lcd_display_string("to database", 2)
-    file.write(str(err))
-    file.close()
-    
-def read_data():
-    global pressure
-    global temperature
-    global humidity
-    try:
-        pressure,temperature1 = sensBMP() #read inf from BMP180
-        humidity, temperature2 = sensDHT() #read inf from DHT22
-        temperature = (temperature1 + temperature2)/2
-        pressure /= 133.322
-        file = open('actual.txt','w')
-        file.write("%.2f"%round(temperature,2))
-        file.write("%.2f"%round(pressure,2))
-        file.write("%.2f"%round(humidity,2))
-        file.close()
-    
-    except Exception as err:  
-        print("Error: ", err)
-        create_log(err,1)
-        
-def write_to_lcd():
-    try:
-        display.lcd_display_string("pressure:"+str("%.2f"%round(pressure,2)), 1)
-        display.lcd_display_string("temp"+str("%.2f"%round(temperature,2))
-                                    +"Hum"+str("%.2f"%round(humidity,2)), 2)
-    except Exception as err:  
-        print("Error: ", err)
-        create_log(err,2)
+    def sensDHT(self):
+        humi = 0
+        temp = 0
+        for i in range(0, 10):
+            humi1, temp1 = Adafruit_DHT.read_retry(self.dhtSensor, self.dhtPin)
+            humi += humi1
+            temp += temp1
+        humi /= 10
+        temp /= 10
+        return humi, temp
 
-def write_to_db():
-    global timestamp
-    timestamp = datetime.datetime.now().timestamp()
-    data = (timestamp,temperature,pressure,humidity)
-    try:
-        sql = """INSERT INTO data
-                (timestamp,temperature,pressure,humidity)
-                VALUES (?,?,?,?)"""
-        cursor.execute(sql, data)
-        
-    except sqlite3.DatabaseError as err:       
-        print("Error: ", err)
-        create_log(err,3)
-    else:
-        conn.commit()
-        
-schedule.every(1).minutes.do(read_data)
-#schedule.every(2).minutes.do(write_to_lcd)
-schedule.every(1).hour.do(write_to_db)
-while True:
-     schedule.run_pending()
-     time.sleep(1)
+    def create_log(self, err, code):
+        with open('err.log', 'w') as file:
+            if code == 1:
+                file.write("РѕС€РёР±РєР° СЃ С‡С‚РµРЅРёРµРј СЃ РґР°С‚С‡РёРєРѕРІ : ")
+                display.lcd_display_string("err: write data", 1)
+                display.lcd_display_string("from sensors", 2)
+            if code == 2:
+                file.write("РѕС€РёР±РєР° СЃ РІС‹РІРѕРґРѕРј РЅР° СЌРєСЂР°РЅ : ")
+            if code == 3:
+                file.write("РѕС€РёР±РєР° СЃ Р·Р°РїРёСЃСЊСЋ РІ Р±Р°Р·Сѓ РґР°РЅРЅС‹С… : ")
+                display.lcd_display_string("err: write data", 1)
+                display.lcd_display_string("to database", 2)
+            file.write(str(err))
+
+    def read_data(self):
+        try:
+            self.pressure, temperature1 = sensBMP()
+            self.humidity, temperature2 = sensDHT()
+            self.temperature = (temperature1 + temperature2) / 2
+            self.pressure /= 133.322
+        except Exception as err:
+            print("Error: ", err)
+            create_log(err, 1)
+
+    def print_data(self, filename=None):
+        if filename is not None:
+            with open(filename, 'w') as file:
+                file.write("%.2f" % round(self.temperature, 2))
+                file.write("%.2f" % round(self.pressure, 2))
+                file.write("%.2f" % round(self.humidity, 2))
+        else:
+            print("temperature: %.2f, pressure: %.2f, humidity: %.2f" % round(self.temperature, 2),
+                  round(self.pressure, 2), round(self.humidity, 2))
+
+    def write_to_lcd(self):
+        try:
+            display.lcd_display_string("pressure: %.2f" % round(self.pressure, 2), 1)
+            display.lcd_display_string("temp %.2f Hum %.2f" % round(self.temperature, 2), round(self.humidity, 2), 2)
+        except Exception as err:
+            print("Error: ", err)
+            create_log(err, 2)
+
+    def write_to_db(self):
+        try:
+            with self.dbConnection.cursor() as cursor:
+                cursor.execute("INSERT INTO data (timestamp,temperature,pressure,humidity) \
+                VALUES (?,?,?,?)", [datetime.now().timestamp(), self.temperature, self.pressure, self.humidity])
+            self.dbConnection.commit()
+        except sqlite3.DatabaseError as err:
+            print("Error: ", err)
+            create_log(err, 3)
+
+
+if __name__ == "__main__":
+    sensors = Sensors("weather_data.db", 4)
+    schedule.every(1).minutes.do(sensors.read_data)
+    schedule.every(1).minutes.do(sensors.print_data)
+    # schedule.every(2).minutes.do(sensors.write_to_lcd)
+    schedule.every(1).hour.do(sensors.write_to_db)
+    while True:
+        schedule.run_pending()
+        time.sleep(10)
--- weather_bot.py
+++ weather_bot.py
@@ -7,13 +7,15 @@ from telepot.loop import MessageLoop
 from telepot.delegate import per_chat_id, create_open, pave_event_space
 
 TOKEN = '698304710:AAGxfMDUIB-wBXnoNGGtFlE9r1UHAkMvj2g'
-admin = 268115509 #admin's id in Telegram
+admin = 268115509  # admin's id in Telegram
+
 
 class Weather_bot(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
-        super(Weather_bot, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.markup = ""
-        self.ttime = 0 
+        self.ttime = 0
+        self.timeDict = {'day': 86400, '3 days': 259200, 'week': 604800, 'mounth': 2592000}
 
     def weather_now(self, pres):
         if pres < 708.81:
@@ -31,8 +33,17 @@ class Weather_bot(telepot.helper.ChatHandler):
         if pres > 787.567:
             return "I hope you are not a farmer, because there will be a strong drought"
         return "odd testimony some wild"
-    
-    def weather_predict(self, delta):
+
+    def weather_predict(self, pressureArray):
+        sumX, sumY, sumX2, sumXY = 0, 0, 0, 0
+        for i in range(0, len(pressureArray)):
+            sumX += i
+            sumX2 += i * i
+            sumY += arr[i]
+            sumXY += arr[i] * i
+        delta = len(arr) * sumXY - sumX * sumY
+        delta /= (len(arr) * sumX2 - sumX * sumX)
+        delta *= len(arr)
         if delta < -150:
             return "the weather will get worse very quickly"
         if -150 <= delta <= -50:
@@ -47,174 +58,131 @@ class Weather_bot(telepot.helper.ChatHandler):
 
     def open(self, initial_msg, seed):
         self.markup = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='data from sensors')],
-        [KeyboardButton(text='weather now')],
-        [KeyboardButton(text='weather predict')],
-        [KeyboardButton(text='statistic')],
+            [KeyboardButton(text='data from sensors')],
+            [KeyboardButton(text='weather now')],
+            [KeyboardButton(text='weather predict')],
+            [KeyboardButton(text='statistic')],
         ])
         hour = datetime.datetime.now().hour
         if 6 <= hour < 12:
-            self.sender.sendMessage ("Good morning",reply_markup=self.markup)
-        elif  12 <= hour < 17:
-            self.sender.sendMessage ("Good day",reply_markup=self.markup)
-        elif  17 <= hour < 23:
-            self.sender.sendMessage ("Good evening",reply_markup=self.markup)
-        elif  23 <= hour or hour < 6:
-            self.sender.sendMessage ("Good night",reply_markup=self.markup)
-        return True 
+            self.sender.sendMessage("Good morning", reply_markup=self.markup)
+        elif 12 <= hour < 17:
+            self.sender.sendMessage("Good day", reply_markup=self.markup)
+        elif 17 <= hour < 23:
+            self.sender.sendMessage("Good evening", reply_markup=self.markup)
+        elif 23 <= hour or hour < 6:
+            self.sender.sendMessage("Good night", reply_markup=self.markup)
+        return True
+
+    def SaveGraphic(self, x, y):
+        plt.plot(x, y)
+        plt.tight_layout()
+        plt.savefig('foo.png', type='png')
+        return 'graphic.png'
 
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         command = msg['text']
         conn = sqlite3.connect("weather_data.db")
         cursor = conn.cursor()
-        
+
         if command == 'data from sensors':
             try:
                 cursor.execute("""select temperature,pressure,
                 humidity from data order by id desc LIMIT 1""")
             except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))
-            temp,pres,humi, = cursor.fetchone()
-            print(temp,pres,humi)
-            self.sender.sendMessage ("temperature: "+str("%.2f"%round(temp,2))+" C")
-            self.sender.sendMessage ("pressure: "+str("%.2f"%round(pres,2))+" mm Hg")
-            self.sender.sendMessage ("humidity: "+str("%.2f"%round(humi,2))+" %")
+                self.bot.sendMessage(admin, "РѕС€РёР±РєР° СЃ С‡С‚РµРЅРёРµРј РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С… " + str(err))
+            else:
+                temp, pres, humi, = cursor.fetchone()
+                self.sender.sendMessage("temperature: %.2f C" % round(temp, 2))
+                self.sender.sendMessage("pressure: %.2f mm Hg" % round(pres, 2))
+                self.sender.sendMessage("humidity: %.2f %" % round(humi, 2))
         elif command == 'weather now':
             try:
                 cursor.execute("select pressure from data order by id desc LIMIT 1")
             except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))    
-            pres, = cursor.fetchone()
-            self.sender.sendMessage ("here weather for now:")
-            self.sender.sendMessage (self.weather_now(pres))
+                self.bot.sendMessage(admin, "РѕС€РёР±РєР° СЃ С‡С‚РµРЅРёРµРј РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С… " + str(err))
+            else:
+                pres, = cursor.fetchone()
+                self.sender.sendMessage("here weather for now:")
+                self.sender.sendMessage(self.weather_now(pres))
         elif command == 'weather predict':
-            arr = list()
+            pressureArray = list()
             timestamp = datetime.datetime.now().timestamp()
-            sql = "select pressure from data where timestamp between "
-            sql += str(timestamp-36000)+" and "+str(timestamp)
+            sql = "select pressure from data where timestamp between {} and {}".format(
+                timestamp - 36000, timestamp)
             try:
                 for row in cursor.execute(sql):
-                    arr.append(row[0])
+                    pressureArray.append(row[0])
             except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))    
-            if len(arr) < 7:
-                self.sender.sendMessage ("Sorry, we don't have actual data right now")
-                self.bot.sendMessage(admin,"Нет данных за последние 10 часов")
+                self.bot.sendMessage(admin, "РѕС€РёР±РєР° СЃ С‡С‚РµРЅРёРµРј РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С… " + str(err))
             else:
-                sumX = 0
-                sumY = 0
-                sumX2 = 0
-                sumXY = 0
-                for i in range(0,len(arr)):
-                    sumX += i
-                    sumX2 += i*i
-                    sumY += arr[i]
-                    sumXY += arr[i]*i
-                delta = len(arr) * sumXY - sumX*sumY
-                delta /= (len(arr)*sumX2 - sumX*sumX)
-                delta *= len(arr)
-                self.sender.sendMessage ("here weather predict for now:")
-                self.sender.sendMessage (self.weather_predict(delta))
+                if len(arr) < 7:
+                    self.sender.sendMessage("Sorry, we don't have actual data right now")
+                    self.bot.sendMessage(admin, "РќРµС‚ РґР°РЅРЅС‹С… Р·Р° РїРѕСЃР»РµРґРЅРёРµ 10 С‡Р°СЃРѕРІ")
+                else:
+                    self.sender.sendMessage("here weather predict for now:")
+                    self.sender.sendMessage(self.weather_predict(pressureArray))
         elif command == 'statistic':
             self.markup = ReplyKeyboardMarkup(keyboard=[
-            [dict(text='day')],
-            [dict(text='3 days')],
-            [dict(text='week')],
-            [dict(text='mounth')],
-            [dict(text='back to main')],
+                [dict(text='day')],
+                [dict(text='3 days')],
+                [dict(text='week')],
+                [dict(text='mounth')],
+                [dict(text='back to main')],
             ])
-            self.sender.sendMessage (command ,reply_markup=self.markup)
-        elif (command == 'day')or(command == '3 days')or(command == 'week')or(command == 'mounth'):
+            self.sender.sendMessage(command, reply_markup=self.markup)
+        elif command in ['day', '3 days', 'week', 'mounth']:
             self.markup = ReplyKeyboardMarkup(keyboard=[
-            [dict(text='temperature')],
-            [dict(text='pressure')],
-            [dict(text='humidity')],
-            [dict(text='back to main')],
+                [dict(text='temperature')],
+                [dict(text='pressure')],
+                [dict(text='humidity')],
+                [dict(text='back to main')],
             ])
-            self.sender.sendMessage (command ,reply_markup=self.markup)
-            if command == 'day' : self.ttime = 86400
-            if command == '3 days' : self.ttime = 259200
-            if command == 'week' : self.ttime = 604800
-            if command == 'mounth' : self.ttime = 2592000
+            self.sender.sendMessage(command, reply_markup=self.markup)
+            self.ttime = self.timeDict[command]
         elif command == 'back to main':
             self.markup = ReplyKeyboardMarkup(keyboard=[
-            [dict(text='data from sensors')],
-            [dict(text='weather now')],
-            [dict(text='weather predict')],
-            [dict(text='statistic')],
+                [dict(text='data from sensors')],
+                [dict(text='weather now')],
+                [dict(text='weather predict')],
+                [dict(text='statistic')],
             ])
-            self.sender.sendMessage (command ,reply_markup=self.markup)
-        elif command == 'temperature':
-            y = list()
-            x = list()
+            self.sender.sendMessage(command, reply_markup=self.markup)
+        elif command in ['temperature', 'pressure', 'humidity']:
+            x, y = [], []
             timestamp = datetime.datetime.now().timestamp()
-            sql = "select timestamp, temperature from data where timestamp between "
-            sql += str(timestamp-self.ttime)+" and "+str(timestamp)
+            sql = "select timestamp, {} from data where timestamp between {} and {}".format(
+                command, timestamp - self.ttime, timestamp)
             try:
                 for row in cursor.execute(sql):
                     x.append(datetime.datetime.fromtimestamp(row[0]))
                     y.append(row[1])
             except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))    
-            plt.figure()
-            plt.plot(x,y)
-            plt.tight_layout()
-            plt.savefig('foo.png')
-            self.sender.sendPhoto (open('foo.png','rb'))
-        elif command == 'pressure':
-            y = list()
-            x = list()
-            timestamp = datetime.datetime.now().timestamp()
-            sql = "select timestamp, pressure from data where timestamp between "
-            sql += str(timestamp-self.ttime)+" and "+str(timestamp)
-            try:
-                for row in cursor.execute(sql):
-                    x.append(datetime.datetime.fromtimestamp(row[0]))
-                    y.append(row[1])
-            except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))    
-            plt.figure()
-            plt.plot(x,y)
-            plt.tight_layout()
-            plt.savefig('foo.png')
-            self.sender.sendPhoto (open('foo.png','rb'))
-        elif command == 'humidity':
-            y = list()
-            x = list()
-            timestamp = datetime.datetime.now().timestamp()
-            sql = "select timestamp, humidity from data where timestamp between "
-            sql += str(timestamp-self.ttime)+" and "+str(timestamp)
-            try:
-                for row in cursor.execute(sql):
-                    x.append(datetime.datetime.fromtimestamp(row[0]))
-                    y.append(row[1])
-            except sqlite3.DatabaseError as err:
-                self.bot.sendMessage(admin,"ошибка с чтением из базы данных "+str(err))    
-            plt.figure()
-            plt.plot(x,y)
-            plt.tight_layout()
-            plt.savefig('foo.png')
-            self.sender.sendPhoto (open('foo.png','rb'))
-        
+                self.bot.sendMessage(admin, "РѕС€РёР±РєР° СЃ С‡С‚РµРЅРёРµРј РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С… " + str(err))
+            else:
+                self.sender.sendPhoto(open(SaveGraphic(x, y), 'rb'))
+
     def on__idle(self, event):
         self.markup = ReplyKeyboardRemove()
-        self.sender.sendMessage ("Good bye" ,reply_markup=self.markup)
+        self.sender.sendMessage("Good bye", reply_markup=self.markup)
         self.close()
 
-bot = telepot.DelegatorBot(TOKEN,[
-    pave_event_space()(
-        per_chat_id(), create_open, Weather_bot, timeout=90),
-])
-MessageLoop(bot).run_as_thread()
-print('Listening ...')
 
-while 1:
-    time.sleep(10)
-    file = open('err.log', 'r')
-    data = file.read()
-    if ( data != ""):
-        bot.sendMessage(admin,data)
-        file.close
-        file = open('err.log', 'w')
-    file.close()
+if __name__ == "__main__":
+    bot = telepot.DelegatorBot(TOKEN, [
+        pave_event_space()(
+            per_chat_id(), create_open, Weather_bot, timeout=90),
+    ])
+    MessageLoop(bot).run_as_thread()
+    print('Listening ...')
+
+    while 1:
+        time.sleep(20)
+        file = open('err.log', 'r')
+        data = file.read()
+        if (data != ""):
+            bot.sendMessage(admin, data)
+            file.close
+            file = open('err.log', 'w')
+        file.close()
